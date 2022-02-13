6b08567b55f2d9b9d52e06a893f116d4157868de
Dushyant Jakhar <44732347+dushyant0007@users.noreply.github.com>
2022-02-13 19:00:03 +0530

    Add files via upload

---
+++ #29_Beautiful_Soup/main.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+# with open("website.html" ) as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents,"html.parser")
+# print(soup.title.string)
+#
+# allTag = soup.find_all(name="a")
+# heading = soup.find(name="h1",id="name")
+
+# response = requests.get("https://academy.bharatingermany.com/p/complete-course-for-studying-in-germany")
+# print(response.text)
+
