6cf840285a52a5108a7c221a8e6bfc72fd323568
Dushyant Jakhar <44732347+dushyant0007@users.noreply.github.com>
2024-08-25 02:07:00 +0530

    Update main.py

--- #28_fligth_deals/main.py
+++ #28_fligth_deals/main.py
@@ -1,21 +1,64 @@
-# 4. Pass the data back to the main.py file, so that you can print the data from main.py
-from data_manager import DataManager
-data_manager = DataManager()
-sheet_data = data_manager.get_destination_data()
-# print(sheet_data)
+# from datetime import datetime, timedelta
+# from data_manager import DataManager
+# from flight_search import FlightSearch
+# from notification_manager import NotificationManager
+#
+#
+# ORIGIN_CITY_IATA = "LON"
+#
+# data_manager = DataManager()
+# flight_search = FlightSearch()
+# notification_manager = NotificationManager()
+#
+# sheet_data = data_manager.get_destination_data()
+#
+# if sheet_data[0]["iataCode"] == "":
+#     city_names = [row["city"] for row in sheet_data]
+#     data_manager.city_codes = flight_search.get_destination_codes(city_names)
+#     data_manager.update_destination_codes()
+#     sheet_data = data_manager.get_destination_data()
+#
+# destinations = {
+#     data["iataCode"]: {
+#         "id": data["id"],
+#         "city": data["city"],
+#         "price": data["lowestPrice"]
+#     } for data in sheet_data}
+#
+# tomorrow = datetime.now() + timedelta(days=1)
+# six_month_from_today = datetime.now() + timedelta(days=6 * 30)
+#
+# for destination_code in destinations:
+#     flight = flight_search.check_flights(
+#         ORIGIN_CITY_IATA,
+#         destination_code,
+#         from_time=tomorrow,
+#         to_time=six_month_from_today
+#     )
+#     print(flight.price)
+#     if flight is None:
+#         continue
+#
+#     if flight.price < destinations[destination_code]["price"]:
+#
+#         users = data_manager.get_customer_emails()
+#         emails = [row["email"] for row in users]
+#         names = [row["firstName"] for row in users]
+#
+#         message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+#
+#         if flight.stop_overs > 0:
+#             message += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
+#
+#         link = f"https://www.google.co.uk/flights?hl=en#flt={flight.origin_airport}.{flight.destination_airport}.{flight.out_date}*{flight.destination_airport}.{flight.origin_airport}.{flight.return_date}"
+#
+#         notification_manager.send_emails(emails, message, link)
+#
+#
 
-#  5. In main.py check if sheet_data contains any values for the "iataCode" key.
-#  If not, then the IATA Codes column is empty in the Google Sheet.
-#  In this case, pass each city name in sheet_data one-by-one
-#  to the FlightSearch class to get the corresponding IATA code
-#  for that city using the Flight Search API.
-#  You should use the code you get back to update the sheet_data dictionary.
-if sheet_data[0]["iataCode"] == "":
-    from flight_search import FlightSearch
-    flight_search = FlightSearch()
-    for row in sheet_data:
-        row["iataCode"] = flight_search.get_destination_code(row["city"])
-    print(f"sheet_data:\n {sheet_data}")
-
-    data_manager.destination_data = sheet_data
-    data_manager.update_destination_codes()
+print("Welcome to Lenar's Flight Club.\nWe find the best flight deals and email you.")
+input("What is your first name?\n")
+input("What is your last name?\n")
+input("What is your email?\n")
+input("Type your email again.\n")
+print("You're in the club!")
